f0326c1b08a1dac5f1995d2e9006834f77d02088
Justin Cally <Justin.Cally@epa.vic.gov.au>
2021-01-11 11:18:38 +1100

    geo filters

---
+++ R/polygonFormat.R
@@ -0,0 +1,38 @@
+#### Format Polygon ####
+#' Simplify polygon
+#'
+#' @param shape 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+polygonFormat <- function(shape, ...) {
+  
+  UseMethod("polygonFormat", shape)
+}
+
+#' Simplify polygon
+#'
+#' @param shape 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+polygonFormat.sf <- function(shape, ...) {
+  
+  shape_crs <- sf::st_crs(shape)
+  shape <- sf::st_union(shape) %>% sf::st_transform(3111)
+  if(hasArg(dTolerance)) {
+    tol <- dTolerance
+  } else {
+    perim <- sf::st_boundary(shape) %>% sf::st_length() 
+    tol <- perim/100
+  }
+  shape %>%
+    sf::st_simplify(dTolerance = tol) %>% 
+    sf::st_transform(shape_crs) 
+}
