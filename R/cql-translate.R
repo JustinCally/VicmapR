48dafa759adb632dd569713cb8851880f4edf780
Justin Cally <jcally@student.unimelb.edu.au>
2021-06-17 20:21:24 +1000

    modifications for cql translate

--- R/cql-translate.R
+++ R/cql-translate.R
@@ -1,3 +1,4 @@
+# Modifications Copyright 2020 Justin Cally
 # Copyright 2019 Province of British Columbia
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -9,7 +10,9 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
-
+#
+# Modifications/state changes made to the original work: 
+# bcdc_dentity() renamed to vicmap_identity()
 
 #' cql translate
 #'
