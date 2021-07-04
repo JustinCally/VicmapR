690163d0bb812b430ac8c8890db13b1855add39b
Justin Cally <jcally@student.unimelb.edu.au>
2021-07-04 18:28:13 +1000

    timeout and vignette building

--- tests/testthat/test-check_geoserver.R
+++ tests/testthat/test-check_geoserver.R
@@ -2,6 +2,6 @@ test_that("Check geoserver works", {
   skip_if_offline()
   expect_error(check_geoserver(timeout = 0.001))
   
-  m <- check_geoserver()
+  m <- check_geoserver(timeout = 20)
   expect_equal(m, "Success: (200) OK")
 })
