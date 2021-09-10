2636e78f094b4bc9255e2ef60e31c0e66bd8f505
Justin Cally <jcally@student.unimelb.edu.au>
2021-09-10 15:50:32 +1000

    add citation

--- R/globals.R
+++ R/globals.R
@@ -10,4 +10,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-utils::globalVariables(c(":=", "name", "type", "."))
+utils::globalVariables(c(":=", "name", "type", ".", ".data"))
