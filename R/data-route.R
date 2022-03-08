c2b4a7120f0d1a03326208efa697ff7532b3d6a5
Rachel Swain <Rachel.Swain@epa.vic.gov.au>
2022-03-08 11:35:43 +1100

    Adds VignetteBuilder field and documents route dataset

---
+++ R/data-route.R
@@ -0,0 +1,16 @@
+#' Cycling Route Data
+#'
+#' A dataframe containing coordinates long a cycling route. Used as example data in the introduction to Vicmap vignette.
+#'
+#' @docType data
+#'
+#' @usage data(route)
+#'
+#' @keywords datasets
+#'
+#'
+#' @source \href{https://ridewithgps.com/routes/4120656}{ridewithGPS}
+#'
+#' @examples
+#' data(route)
+"route"
