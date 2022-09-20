30f8cf1aa5d905d638f045814a670a3f612afcde
Justin Cally <jcally@student.unimelb.edu.au>
2022-09-20 10:51:18 +1000

    test other functions (select, geometric filter)

--- tests/testthat/test-migration.R
+++ tests/testthat/test-migration.R
@@ -2,6 +2,12 @@ base.url <- getOption("vicmap.base_url", default = VicmapR:::base_wfs_url)
 new.url <- "https://opendata-uat.maps.vic.gov.au/geoserver/wfs"
 options(vicmap.base_url = new.url)
 options(vicmap.backend = "AWS")
+melbourne <- sf::st_read(system.file("shapes/melbourne.geojson", package="VicmapR"), quiet = T)
+poi <- data.frame(x = 144.7250, 
+                  y = -36.2894) %>%
+  sf::st_as_sf(., coords = c("x", "y"), crs = 4283) %>%
+  sf::st_transform(3111) %>%
+  sf::st_buffer(dist = 2500)
 
 test_that("new url works", {
   testthat::expect_true(check_geoserver())
@@ -11,5 +17,26 @@ test_that("listLayers url works", {
   testthat::expect_equal(class(listLayers()), "data.frame")
 })
 
-options(vicmap.base_url = base.url)
+test_that("filter and feature hits works", {
+  testthat::expect_equal(vicmap_query("open-data-platform:extent_100y_ari") %>% 
+                           filter(method == "Mixed") %>% 
+                           feature_hits(), 1)
+})
+
+test_that("geometric filter works", {
+  
+  testthat::expect_equal(vicmap_query("open-data-platform:extent_100y_ari") %>% 
+                           filter(INTERSECTS(poi)) %>% 
+                           feature_hits(), 2)
+})
+
+test_that("select works", {
+  
+  testthat::expect_equal(ncol(vicmap_query("open-data-platform:extent_100y_ari") %>% 
+                           select(method) %>% 
+                           head(1) %>%
+                           collect()), 3)
+})
+
+options(vicmap.base_url = VicmapR:::base_wfs_url)
 
